2a819fe27f16e3ddc4e9e778bb28514093e76a1b
rehan <rehanmd705063@gmail.com>
2024-11-13 02:01:20 +0530

    Upload Linktree

---
+++ linkTree/script.js
@@ -0,0 +1,19 @@
+document.getElementById("portfolio").addEventListener("click", () => {
+    window.location = "https://portfolio2114.netlify.app";
+})
+
+document.getElementById("linkedin").addEventListener("click", () => {
+    window.location = "https://www.linkedin.com/in/md-rehan-98782a274";
+})
+
+document.getElementById("instagram").addEventListener("click", () => {
+    window.location = "https://www.instagram.com/rehan70506/profilecard/?igsh=MW95MDNvd3ozdGwxYQ==";
+})
+
+document.getElementById("twitter").addEventListener("click", () => {
+    window.location = "https://x.com/MdRehan70506?t=qvkFocpFGk5C6XlxkSKB7w&s=09";
+})
+
+document.getElementById("contact").addEventListener("click", () => {
+    window.location = "contact.html";
+})
